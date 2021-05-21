d77cdbd6c9297d61f33a14c0554950662f01e47a
Matthew Flancer <maf291@pitt.edu>
2021-05-21 01:21:19 -0400

    started getRoster function and added helper functions

--- mainScraper.py
+++ mainScraper.py
@@ -5,8 +5,6 @@ import pandas as pd
 import json
 import time
 
-#test
-
 teams = pd.read_csv('https://raw.githubusercontent.com/maflancer/CollegeSwimmingScraper/main/collegeSwimmingTeams.csv')
 
 #function that takes as an input team names, a division, or a conference and returns the teams that match the input
@@ -24,18 +22,57 @@ def getTeams(team_names = ['NONE'], division_names = ['NONE'], conference_names 
 
 	return team_df
 
-#function that takes a team and either a season_ID or year as an input and returns the team's roster from that year
-def getRoster(team, season_ID = -1, year = -1):
-	#TODO
-	print('')
+#changes name from (last, first) to (first last)
+def cleanName(webName):
+	nameList = webName.split(', ')
+	last_name = nameList[0]
+	first_name = nameList[1]
+
+	return first_name + ' ' +  last_name
+
+#extracts swimmer id from href
+def getSwimmerID(href):
+	return href.split('/')[-1]
+
+#function that takes a team and gender, and either a season_ID or year as an input and returns the team's roster from that year
+#example function call - getRoster(team = "University of Pittsburgh", gender = "M") - if no season or year -> returns one dataframe with roster for each season from 2010 - 2021
+#                      - getRoster(team = "University of Pittsburgh", gender = "F", year = 2020) - roster for 2020-2021 team corresponds to season #24
+def getRoster(team, gender, season_ID = -1, year = -1):
+	team_row = teams[teams['team_name'] == team]
+
+	#team_number = (team_row['team_ID'].str.split())[1]
+
+	#print(team_number)
+
+	roster_url = 'https://www.swimcloud.com/team/' + str(405) +  '/roster/?page=1&gender=' + gender + '&season_id=' + str(24)
+
+	url = requests.get(roster_url, headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36', 'Referer' : 'https://google.com/'})
+
+	url.encoding = 'utf-8'
+
+	soup = bs(url.text, 'html.parser')
+
+	data = soup.find('table', attrs = {'class' : 'c-table-clean c-table-clean--middle table table-hover'}).find_all('tr')[1:] #finds table of player names
+
+	for row in data:
+		print(cleanName(row.find('a').text.strip()))
+		idArray = row.find_all('a') 				 #returns array of length 1 which contains swimmer ID
+		swimmer_id = getSwimmerID(idArray[0]['href'])
+		numbers = row.find_all('td')
+		grade = numbers[3].text.strip()
+
+		print(swimmer_id)
+		print(grade)
 
 
 #tests
-df = getTeams(team_names = ['University of Pittsburgh', 'University of Louisville'])
-print(df.head())
+#df = getTeams(team_names = ['University of Pittsburgh', 'University of Louisville'])
+#print(df.head())
 
-df1 = getTeams(conference_names = ['ACC', 'Ivy'])
-print(df1)
+#df1 = getTeams(conference_names = ['ACC', 'Ivy'])
+#print(df1)
 
-df2 = getTeams(division_names = ['Division 1'])
-print(df2.head())
+#df2 = getTeams(division_names = ['Division 1'])
+#print(df2.head())
+
+getRoster(team = "University of Pittsburgh", gender = "M", year = 2020)
